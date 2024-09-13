surendrailla36/Bitcoin

rpc: Improve gettxoutproof error when only header is available.

src/rpc/txoutproof.cpp
@@ -10,6 +10,7 @@
 #include <merkleblock.h>
 #include <node/blockstorage.h>
 #include <primitives/transaction.h>
+#include <rpc/blockchain.h>
 #include <rpc/server.h>
 #include <rpc/server_util.h>
 #include <rpc/util.h>
@@ -96,6 +97,10 @@ static RPCHelpMan gettxoutproof()
                 }
             }
 
+            {
+                LOCK(cs_main);
+                CheckHeaderForData(chainman.m_blockman, *pblockindex, /*check_for_undo=*/false);
+            }
             CBlock block;
             if (!chainman.m_blockman.ReadBlockFromDisk(block, *pblockindex)) {
                 throw JSONRPCError(RPC_INTERNAL_ERROR, "Can't read block from disk");

SashimiSaketoro/Freycoin

src/rpc/txoutproof.cpp
@@ -10,6 +10,7 @@
 #include <merkleblock.h>
 #include <node/blockstorage.h>
 #include <primitives/transaction.h>
+#include <rpc/blockchain.h>
 #include <rpc/server.h>
 #include <rpc/server_util.h>
 #include <rpc/util.h>
@@ -96,6 +97,10 @@ static RPCHelpMan gettxoutproof()
                 }
             }
 
+            {
+                LOCK(cs_main);
+                CheckBlockDataAvailability(chainman.m_blockman, *pblockindex, /*check_for_undo=*/false);
+            }
             CBlock block;
             if (!chainman.m_blockman.ReadBlockFromDisk(block, *pblockindex)) {
                 throw JSONRPCError(RPC_INTERNAL_ERROR, "Can't read block from disk");